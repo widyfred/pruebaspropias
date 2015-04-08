9c771a21c0bb3041464928d1911ddccd8bd5d288
MadbaM <jdloferro@gmail.com>
2015-04-08 19:51:34 +0200

    primer commit

---
+++ src/ejer-01-tema-12.cpp
@@ -0,0 +1,15 @@
+//============================================================================
+// Name        : ejer-01-tema-12.cpp
+// Author      : 
+// Version     :
+// Copyright   : Your copyright notice
+// Description : Hello World in C++, Ansi-style
+//============================================================================
+
+#include <iostream>
+using namespace std;
+
+int main() {
+	cout << "!!!Hello World!!!" << endl; // prints !!!Hello World!!!
+	return 0;
+}
